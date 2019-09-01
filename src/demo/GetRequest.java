166bf7a50fae64b39bc9cc762dbd9120ab841e3b
Rajiv Singh <rajivpaulsingh@gmail.com>
2019-09-01 16:00:04 -0700

    Sample code for post request

--- src/demo/GetRequest.java
+++ src/demo/GetRequest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
-public class Basics {
+public class GetRequest {
 
     @Test
     public void Test1() {
