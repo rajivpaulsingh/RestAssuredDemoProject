e8d7b6ca248132e1a2c2dcfed766ad7d8ca70e87
Singh <singh2rajiv@dgb-c02vh08bh.bcg.com>
2020-07-29 17:00:35 -0700

    Added few more important files

--- src/files/PayLoad.java
+++ src/files/PayLoad.java
@@ -1,52 +1,71 @@
 package files;
 
-public class PayLoad {
+public class payload {
 
-    public static String getPostData() {
-
-        String b = "{\n" +
-                "    \"location\":{\n" +
-                "        \"lat\" : -38.383494,\n" +
-                "        \"lng\" : 33.427362\n" +
-                "    },\n" +
-                "    \"accuracy\":50,\n" +
-                "    \"name\":\"Frontline house\",\n" +
-                "    \"phone_number\":\"(+91) 983 893 3937\",\n" +
-                "    \"address\" : \"29, side layout, cohen 09\",\n" +
-                "    \"types\": [\"shoe park\",\"shop\"],\n" +
-                "    \"website\" : \"http://google.com\",\n" +
-                "    \"language\" : \"French-IN\"\n" +
-                "}";
-
-        return b;
-    }
-
-    public static String getDeleteData(String placeID) {
-
-        String d = "{\n" +
-                "    \"place_id\":\"" + placeID + "\" \n" +
-                "}";
-        return d;
-    }
-
-    public static String addBook(String isbn, String aisle) {
-
-        String book = "{\n" +
-                "\"name\":\"Learn Appium Automation with Java\",\n" +
-                "\"isbn\":\"" + isbn + "\",\n" +
-                "\"aisle\":\"" + aisle + "\",\n" +
-                "\"author\":\"John foe\"\n" +
-                "}";
-
-        return book;
-    }
-
-    public static String deleteBook(String isbnaisle) {
-
-        String book = "{\n" +
-                "\"ID\" : \"" + isbnaisle + "\"\n" +
-                "}";
-        return book;
-    }
-
+	
+	public static String AddPlace()
+	
+	{
+		return "{\r\n" + 
+				"  \"location\": {\r\n" + 
+				"    \"lat\": -38.383494,\r\n" + 
+				"    \"lng\": 33.427362\r\n" + 
+				"  },\r\n" + 
+				"  \"accuracy\": 50,\r\n" + 
+				"  \"name\": \"Rahul Shetty Academy\",\r\n" + 
+				"  \"phone_number\": \"(+91) 983 893 3937\",\r\n" + 
+				"  \"address\": \"29, side layout, cohen 09\",\r\n" + 
+				"  \"types\": [\r\n" + 
+				"    \"shoe park\",\r\n" + 
+				"    \"shop\"\r\n" + 
+				"  ],\r\n" + 
+				"  \"website\": \"http://rahulshettyacademy.com\",\r\n" + 
+				"  \"language\": \"French-IN\"\r\n" + 
+				"}\r\n" + 
+				"";
+		
+		
+	}
+	
+	public static String CoursePrice()
+	{
+		
+		return "{\r\n" + 
+				"  \"dashboard\": {\r\n" + 
+				"    \"purchaseAmount\": 1162,\r\n" + 
+				"    \"website\": \"rahulshettyacademy.com\"\r\n" + 
+				"  },\r\n" + 
+				"  \"courses\": [\r\n" + 
+				"    {\r\n" + 
+				"      \"title\": \"Selenium Python\",\r\n" + 
+				"      \"price\": 50,\r\n" + 
+				"      \"copies\": 6\r\n" + 
+				"    },\r\n" + 
+				"    {\r\n" + 
+				"      \"title\": \"Cypress\",\r\n" + 
+				"      \"price\": 40,\r\n" + 
+				"      \"copies\": 4\r\n" + 
+				"    },\r\n" + 
+				"    {\r\n" + 
+				"      \"title\": \"RPA\",\r\n" + 
+				"      \"price\": 45,\r\n" + 
+				"      \"copies\": 10\r\n" + 
+				"    },\r\n" + 
+				"     {\r\n" + 
+				"      \"title\": \"Appium\",\r\n" + 
+				"      \"price\": 36,\r\n" + 
+				"      \"copies\": 7\r\n" + 
+				"    }\r\n" + 
+				"    \r\n" + 
+				"    \r\n" + 
+				"    \r\n" + 
+				"  ]\r\n" + 
+				"}\r\n" + 
+				"";
+		
+		
+		
+}
+	
+	
 }
